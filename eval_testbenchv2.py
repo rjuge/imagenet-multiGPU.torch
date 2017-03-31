4e3bdb8366bfb70c1c3268d3a9aeb554e8d5061f
remi <gingy.juge@gmail.com>
2017-03-31 18:55:41 +0200

    improvements of testbench eval script

--- eval_testbenchv2.py
+++ eval_testbenchv2.py
@@ -1,12 +1,8 @@
 import os
 import numpy as np
-import scipy as sp
-from scipy import misc
 import lutorpy as lua
 import argparse
 import itertools
-from PIL import Image
-from skimage import img_as_float
 from skimage import transform
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
@@ -14,7 +10,6 @@ from pylab import *
 
 np.set_printoptions(precision=2)
 
-#require('torch')
 require('nn')
 require('cunn')
 require('cudnn')
@@ -30,47 +25,10 @@ if(args.testbench == 'horus'):
 elif(args.testbench == 'raw'):
     path = "/home/remi/Deep_Learning/objRecTestbench/dataset/tiny_hv02_testbench/"
 
-top1 = 0.0
-top3 = 0.0
-top5 = 0.0
-
 file = open('./testbench_results/py_testbench_'+args.model[:-3]+'.txt', 'w')
 
 file.write('Testbench results for model :'+args.model+'\n')
 file.write('\n')
-
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
 
 def imgPreProcess(im):
     im = transform.resize(im, (3,224,224), preserve_range=True)
@@ -103,6 +61,10 @@ model.convnet = model.convnet._cuda()
 
 print model.convnet
 
+top1 = 0.0
+top3 = 0.0
+top5 = 0.0
+
 top1_classes = np.ndarray(shape=(400,))
 top3_classes = np.ndarray(shape=(400,))
 top5_classes = np.ndarray(shape=(400,))
@@ -111,6 +73,10 @@ nTest = 0
 predictions = []
 truth = []
 classes = []
+
+correct=[]
+incorrect=[]
+
 for subdir, _, files in os.walk(path):
     if(subdir == path):
         continue
@@ -120,6 +86,7 @@ for subdir, _, files in os.walk(path):
     top5_class = 0.0
     n_class = 0
     classes.append(int(subdir[-3:]))
+    
     for f in files:
         nTest += 1
         n_class += 1
@@ -139,9 +106,13 @@ for subdir, _, files in os.walk(path):
         predictions.append(pred_sorted[0])
         label = int(subdir[-3:])
         truth.append(label)
+        
         if(pred_sorted[0] == label):
             top1 = top1 + 1 
             top1_class = top1_class + 1
+            correct.append(pred[pred_sorted[0]-1])
+        else:
+            incorrect.append(pred[pred_sorted[0]-1])
         if(isTop3(pred_sorted, label) == True): 
             top3 = top3 + 1
             top3_class = top3_class + 1
@@ -161,42 +132,57 @@ top1 = top1 * 100 / nTest
 top3 = top3 * 100 / nTest
 top5 = top5 * 100 / nTest
 
+#confusion matrix
+cm = confusion_matrix(truth, predictions, labels=classes)
+
 file.write('Global Top1: '+str(top1)+'\n')
 file.write('Global Top3: '+str(top3)+'\n')
 file.write('Global Top5: '+str(top5)+'\n')
 file.write('\n')
+file.write('TP:',tp,'\n')
+file.write('TN:',tn,'\n')
+file.write('FP:',fp,'\n')
+file.write('FN:',fn,'\n')
+file.write('\n')
 file.write('Scores per class:'+'\n')
-file.write('hv02_ Top1 Top3 Top5'+'\n')
-file.write('---------------------'+'\n')
-for i in sorted(range(len(top1_classes)), key=lambda k: top1_classes[k], reverse=True):
-    file.write(str(i+1)+' '+str(top1_classes[i])+' '
+file.write(' Class  Top1  Top3  Top5'+'\n')
+file.write('------------------------'+'\n')
+
+top1_id = sorted(range(len(top1_classes)), key=lambda k: top1_classes[k], reverse=True)
+for i in top1_id:
+    file.write('hv02_'+(str(i+1).zfill(3))+' '+str(top1_classes[i])+' '
                +str(top3_classes[i])+' '+str(top5_classes[i])+' '+'\n')
 
-cm = confusion_matrix(truth, predictions, labels=classes)
-
-#f = plt.figure()
-plot_confusion_matrix(cm, classes, normalize=False,
-                      title='Normalized confusion matrix')
-
-#plt.savefig('CM.png', format='png')
-
-top1_sorted = sorted(top1_classes, reverse=True)
-top1_id = sorted(range(len(top1_classes)), key=lambda k: top1_classes[k], reverse=True)
-pos = np.arange(400)+.5
-f, ax = plt.subplots()
-ax.barh(pos, top1_sorted, align='center')
+#top1 chart
+bar_sorted = sorted(top1_classes)
+bar_id = sorted(range(len(top1_classes)), key=lambda k: top1_classes[k])
+bar_id = ['hv02_'+(str(i+1).zfill(3)) for i in bar_id]
+pos = np.arange(len(bar_id))
+f, ax  = plt.subplots(figsize=(50, 50))
+ax.barh(pos, bar_sorted, height=0.8, align='center')
 ax.set_yticks(pos)
-ax.set_yticklabels(top1_id)
-ax.invert_yaxis()
+ax.set_yticklabels(bar_id)
 ax.set_xlabel('Top1')
 ax.set_title('Top1 chart')
-ax.set_ylim(bottom=0, top=500)
-grid(True)
-plt.savefig('hist.png', format='png')
+ax.set_ylim(bottom=0, top=401)
+plt.savefig('./testbench_results/top1_chart_'+args.model[:-3]+'.png', format='png')
+
+#histograms
+fig = plt.figure()
+plt.hist(np.asarray(correct), 50, facecolor=(0, 0, 1, 0.5))
+plt.hist(np.asarray(incorrect), 50, facecolor=(1, 0, 0, 0.5))
+plt.axis([0.0, 1.0, 0, 250])
+plt.title('Correct/Incorrect labels')
+plt.xlabel('Confidences')
+plt.ylabel('Count')
+plt.grid(True)
+plt.savefig('./testbench_results/hist_'+args.model[:-3]+'.png', format='png')
+
+
 print('==> Test ran on '+str(nTest)+' samples')
 
-print 'Top1 ',top1
-print('Top3 '+str(top3))
-print('Top5 '+str(top5))
+print 'Top1',top1
+print 'Top3',top3
+print 'Top5',top5
 
 file.close()
