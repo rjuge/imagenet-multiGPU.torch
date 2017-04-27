a5e92e9b6ee1cb7fc4d201aa675e76c2d9678e7d
Remi <gingy.juge@gmail.com>
2017-04-27 17:14:12 +0200

    Fix

--- models/aipolynet.lua
+++ models/aipolynet.lua
@@ -16,18 +16,15 @@ function residualWithConv_block(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW,
 local concat = nn.ConcatTable()
 local m = nn.Sequential()
 local res = nn.Sequential()
-m:add(nn.SpatialConvolution(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH))
+m:add(nn.SpatialConvolution(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH):noBias())
 m:add(nn.SpatialBatchNormalization(nOutputPlane, 1e-3))
 m:add(nn.ReLU(true))
-m:add(nn.SpatialConvolution(nOutputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH):noBias())
+m:add(nn.SpatialConvolution(nOutputPlane, nOutputPlane, kW, kH, dW/2, dH/2, padW, padH):noBias())
 m:add(nn.SpatialBatchNormalization(nOutputPlane, 1e-3))
 m:add(nn.SpatialDropout(0.1))
 res:add(nn.SpatialConvolution(nInputPlane, nOutputPlane, 1, 1, 2, 2):noBias())
 res:add(nn.SpatialBatchNormalization(nOutputPlane, 1e-3))
 res:add(nn.SpatialDropout(0.01))
-if  nInputPlane ~= nOutputPlane  then
-      res:add(nn.Padding(1, nOutputPlane-nInputPlane, 3))
-end
 concat:add(m):add(res)
 return nn.Sequential():add(concat):add(nn.CAddTable()):add(nn.ReLU(true))
 end
@@ -46,36 +43,36 @@ model:add(nn.ReLU(true))
 -- first block
 local block1 = nn.Sequential()
 block1:add(residualWithConv_block(16, 32, 3, 3, 2, 2, 1, 1))
-block1:add(residual_block(32, 32, 3, 3, 2, 2, 1, 1))
-block1:add(residual_block(32, 32, 3, 3, 2, 2, 1, 1))
+block1:add(residual_block(32, 32, 3, 3, 1, 1, 1, 1))
+block1:add(residual_block(32, 32, 3, 3, 1, 1, 1, 1))
 
 -- second block
 local block2 = nn.Sequential()
 block2:add(residualWithConv_block(32, 32, 3, 3, 2, 2, 1, 1))
-block2:add(residual_block(32, 32, 3, 3, 2, 2, 1, 1))
-block2:add(residual_block(32, 32, 3, 3, 2, 2, 1, 1))
-block2:add(residual_block(32, 32, 3, 3, 2, 2, 1, 1))
+block2:add(residual_block(32, 32, 3, 3, 1, 1, 1, 1))
+block2:add(residual_block(32, 32, 3, 3, 1, 1, 1, 1))
+block2:add(residual_block(32, 32, 3, 3, 1, 1, 1, 1))
 
 -- third block
 local block3 = nn.Sequential()
 block3:add(residualWithConv_block(32, 64, 3, 3, 2, 2, 1, 1))
-block3:add(residual_block(64, 64, 3, 3, 2, 2, 1, 1))
-block3:add(residual_block(64, 64, 3, 3, 2, 2, 1, 1))
-block3:add(residual_block(64, 64, 3, 3, 2, 2, 1, 1))
-block3:add(residual_block(64, 64, 3, 3, 2, 2, 1, 1))
-block3:add(residual_block(64, 64, 3, 3, 2, 2, 1, 1))
+block3:add(residual_block(64, 64, 3, 3, 1, 1, 1, 1))
+block3:add(residual_block(64, 64, 3, 3, 1, 1, 1, 1))
+block3:add(residual_block(64, 64, 3, 3, 1, 1, 1, 1))
+block3:add(residual_block(64, 64, 3, 3, 1, 1, 1, 1))
+block3:add(residual_block(64, 64, 3, 3, 1, 1, 1, 1))
 
 -- fourth block
 local block4 = nn.Sequential()
 block4:add(residualWithConv_block(64, 128, 3, 3, 2, 2, 1, 1))
-block4:add(residual_block(128, 128, 3, 3, 2, 2, 1, 1))
-block4:add(residual_block(128, 128, 3, 3, 2, 2, 1, 1))
+block4:add(residual_block(128, 128, 3, 3, 1, 1, 1, 1))
+block4:add(residual_block(128, 128, 3, 3, 1, 1, 1, 1))
 
 model:add(block1):add(block2):add(block3):add(block4)
 model:add(nn.Identity())
 
 -- global average pooling 1x1
-model:add(nn.SpatialAveragePooling(14, 14, 1, 1, 0, 0))
+model:add(nn.SpatialAveragePooling(7, 7, 1, 1, 0, 0))
 model:add(nn.View(-1):setNumInputDims(3))
 
 local m = nn.Sequential()
